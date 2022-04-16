d2549446063aade38823df1ba1d5c88f5e75ddf1
Douglas Higashi <higoshi.douglas@gmail.com>
2022-04-16 19:04:15 -0300

    Camera system and movement along player implemented

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -102,7 +102,14 @@ public class TileManager {
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 			
-			g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+			boolean inScreen = (worldX + gp.tileSize> gp.player.worldX - gp.player.screenX &&
+								worldX - gp.tileSize< gp.player.worldX + gp.player.screenX && 
+								worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+								worldY - gp.tileSize< gp.player.worldY + gp.player.screenY);
+			
+			if(inScreen) {
+				g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+			}
 			worldCol++;
 			
 			if(worldCol == gp.maxWorldCol) {
